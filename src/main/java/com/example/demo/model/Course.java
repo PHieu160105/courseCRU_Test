8b322cb79fc6920c45cbbc52c02bc5c8068d2a28
Pham Huy Hieu <hieuhuy160105@gmail.com>
2025-09-15 19:54:10 +0700

    fixing pom.xml problems relating lombok

--- src/main/java/com/example/demo/model/Course.java
+++ src/main/java/com/example/demo/model/Course.java
@@ -8,6 +8,10 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "courses")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,46 +25,4 @@ public class Course {
 
     @Column(name = "email")
     private String email;
-
-    public Course() {
-    }
-
-    public Course(Long id, String courseName, String instructor, String email) {
-        this.id = id;
-        this.courseName = courseName;
-        this.instructor = instructor;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public String getInstructor() {
-        return instructor;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public void setInstructor(String instructor) {
-        this.instructor = instructor;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
